99702199249f1ae071ea4dfdeb5fb9a1b288c1ef
Rosangela <roseffc@gmail.com>
2023-10-13 22:35:50 -0300

    feat: criado component card

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CardComponent } from 'src/app/shared/components/card/card.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HomeComponent]
+  imports: [CommonModule],
+
+  declarations: [HomeComponent,
+    CardComponent]
 })
 
 export class HomeModule { }
