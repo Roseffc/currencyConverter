7dbea235a990e566dd0e8f9185158ad26341ec69
Rosangela <roseffc@gmail.com>
2023-10-15 19:40:24 -0300

    refactor: convertido valor de string para number

--- src/app/shared/components/status-currency/status-currency.component.ts
+++ src/app/shared/components/status-currency/status-currency.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-status-currency',
   templateUrl: './status-currency.component.html',
-  styleUrls: ['./status-currency.component.scss']
+  styleUrls: ['./status-currency.component.scss'],
 })
 export class StatusCurrencyComponent implements OnInit {
-  @Input() coin=0;
+  private currencyValue: number = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  @Input()
+  set coin(value: any) {
+    if (typeof value === 'string') {
+      this.currencyValue = parseFloat(value);
+    } else {
+      this.currencyValue = value;
+    }
   }
 
+  get manipulatedValue(): number {
+    return this.currencyValue;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
