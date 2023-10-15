bcfa6e0e48349b7ae310df70c90269972e6ced44
Rosangela <roseffc@gmail.com>
2023-10-15 19:33:09 -0300

    refactor: component card e ajustes responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,36 @@
+import { CurrencyQuoteType } from 'src/app/types/currency-quote';
+import { CurrencyQuoteService } from './../../services/currency-quote.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  currencyCodes = ['CAD-BRL', 'ARS-BRL', 'GBP-BRL'];
+  currencyQuote!: CurrencyQuoteType;
 
-  constructor() { }
+  constructor(private currencyQuoteService: CurrencyQuoteService) {}
 
   ngOnInit() {
+    this.getCurrencyQuote();
   }
 
+  getCurrencyQuote() {
+    this.currencyQuoteService
+      .getCurrencyQuote(this.currencyCodes)
+      .subscribe((result) => {
+        this.currencyQuote = result;
+        console.log(result);
+      });
+  }
+
+  getBidValue(item: any) {
+    return item.bid;
+  }
+
+  getCurrencyName(item: any) {
+    return item.name.split('/')[0];
+  }
 }
